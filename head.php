e432c2dc0fca2093adc5e3ced05036d5369c5c9f
Peter Paravinja <paravinja92@gmail.com>
2020-04-14 22:06:08 +0200

    Added a nice favicon.

--- head.php
+++ head.php
@@ -5,4 +5,5 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<link rel="stylesheet"  href="assets/css/style.css">
 	<script src="assets/js/script.js"></script>
+	<link rel='icon' href='./assets/icons/viking.ico' type='image/x-icon'/ >
 </head>
